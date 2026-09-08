827163fe09719d16402fbff0cd2ac664666172a7
rifkix <rifkix@gmail.com>
2026-09-08 09:47:48 +0700

    praktikum aplro pekan 2 selasa

--- pekan2/Konstanta_2511533007.py
+++ pekan2/Konstanta_2511533007.py
@@ -1,6 +1,6 @@
 from typing import Final
-PI: Final = 3.14
-print("pi: %f" % (PI))
-jari = float(input("Masukkan nilai jari-jari: "))
-luas = PI * jari * jari
-print("Luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari, luas))
+PI_3007: Final = 3.14
+print("pi: %f" % (PI_3007))
+jari_3007 = float(input("Masukkan nilai jari-jari: "))
+luas_3007 = PI_3007 * jari_3007 * jari_3007
+print("Luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari_3007, luas_3007))
