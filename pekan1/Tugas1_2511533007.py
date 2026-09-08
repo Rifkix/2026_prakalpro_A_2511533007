e59139336ffa7e46b59b130d502774b429fb51e8
rifkix <rifkix@gmail.com>
2026-09-08 21:31:47 +0700

    tugas dan laprak

---
+++ pekan1/Tugas1_2511533007.py
@@ -0,0 +1,5 @@
+#Program menghitung luas segitiga
+alas_3007 = float(input("Alas: "))
+tinggi_3007 = float(input("Tinggi: "))
+luas_segitiga_3007 = alas_3007 * tinggi_3007 / 2
+print("Luas segitiga yaitu: ", luas_segitiga_3007)
